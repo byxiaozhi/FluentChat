83f33908e453f46e26a54cf0b285d9f6f409c0dc
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-06-14 02:03:52 +0800

    添加创建群页面

---
+++ FluentChat/CreateGroup.xaml.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "winrt/Microsoft.UI.Xaml.h"
+#include "winrt/Microsoft.UI.Xaml.Markup.h"
+#include "winrt/Microsoft.UI.Xaml.Controls.Primitives.h"
+#include "CreateGroup.g.h"
+
+namespace winrt::FluentChat::implementation
+{
+    struct CreateGroup : CreateGroupT<CreateGroup>
+    {
+        CreateGroup();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::FluentChat::factory_implementation
+{
+    struct CreateGroup : CreateGroupT<CreateGroup, implementation::CreateGroup>
+    {
+    };
+}
